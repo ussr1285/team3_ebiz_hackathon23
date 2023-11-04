a1d5ac1ca45a3c8684d5b2b77a9152537a92bee8
TJWorld01 <116538001+TJWorld01@users.noreply.github.com>
2023-11-04 12:07:16 +0900

    Add files via upload

---
+++ 5/5-2.py
@@ -0,0 +1,11 @@
+5-2
+
+for i in range(7): #층에 따라 range를 맞춤
+    if 8-(2*i) >= 2: #stars의 개수가 2이상이면 첫번째 식
+        spaces = ' ' * (i)
+        stars = '*' * (8 - (2*i))
+    else: #stars의 개수가 2 미만일땐 두번째 식 적용
+        spaces = ' ' * (6- i)
+        stars = '*' * (2*i - 4)
+    line = spaces + stars + spaces
+    print(line)
